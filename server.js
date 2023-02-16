6b17878ecf4d343fb4e5ec574375cb66e23f847e
Bektur12 <kanybekovbektur22@gmail.com>
2023-02-16 17:22:04 +0600

    added get method in database

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const studentRoutes = require("./src/student/routes");
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send({ message: "hello beka" });
 });
+
+app.use("/api/v1/students", studentRoutes);
 
 app.listen(port, () => console.log("molodess"));
--- src/student/db.js
+++ src/student/db.js
@@ -1,3 +1,11 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool();
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "students",
+  password: "bekaseka",
+  port: 5432,
+});
+
+module.exports = pool;
